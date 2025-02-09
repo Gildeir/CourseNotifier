ac4e6d34eab139b386e6b653fa2d58ae316a60d7
gildeir <gildeirlopes@gmail.com>
2025-02-09 16:10:21 -0300

    Add project files.

---
+++ ContentContext/Article.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Balta.ContentContext
+{
+    public class Article:Content
+    {
+        public string Name { get; set; }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using Balta.ContentContext;
+
+namespace Balta
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var article = new Article();
+
+            article.Name = "yep";
+        }
+    }
+}
